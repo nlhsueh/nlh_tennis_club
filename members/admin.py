6228e78fa6ab73cf58c6ec2342283437cbbff30a
nienlinh <nlhsueh@mail.fcu.edu.tw>
2023-12-06 21:01:17 +0800

    just like w3school sample code

---
+++ members/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Member
+
+# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+
+admin.site.register(Member, MemberAdmin)
+
+# admin.site.register(Member)
+
+
