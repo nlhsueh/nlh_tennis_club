bb3ec10b959ae833dd3b3359bea4782a40532376
nienlinh <nlhsueh@mail.fcu.edu.tw>
2023-12-19 10:17:13 +0800

    bs form

--- web/forms.py
+++ web/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=20)
-    password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    username = forms.CharField(max_length=20, 
+                               widget=forms.TextInput(attrs={'class': 'form-control col-md-3'}))
+    password = forms.CharField(max_length=20, 
+                               widget=forms.PasswordInput(attrs={'class': 'form-control col-md-3'}))
