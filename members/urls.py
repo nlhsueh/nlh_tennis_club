adc535722cd8d42ac9393bba0d0c336f50eb7865
nienlinh <nlhsueh@mail.fcu.edu.tw>
2024-05-06 16:45:18 +0800

    members

--- members/urls.py
+++ members/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.members, name='members'),
     path('members/', views.members, name='members'),
-    path('members/details/<int:id>', views.details, name='details'),
 ]
 
 print ('urlpatterns in called in members', urlpatterns)
--- members/views.py
+++ members/views.py
@@ -13,11 +13,3 @@ def members(request):
     'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
-
-def details(request, id):
-  mymember = Member.objects.get(id=id)
-  template = loader.get_template('details.html')
-  context = {
-    'mymember': mymember,
-  }
-  return HttpResponse(template.render(context, request))
