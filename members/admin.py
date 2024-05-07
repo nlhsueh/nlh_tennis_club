9c22ba35a717a97c97621f211342085e674b4d18
nienlinh <nlhsueh@mail.fcu.edu.tw>
2024-05-07 20:49:20 +0800

    admin

--- members/admin.py
+++ members/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Member
 
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+  
+admin.site.register(Member, MemberAdmin)
 
 
--- members/models.py
+++ members/models.py
@@ -1,9 +1,22 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Member(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255,
+                                 verbose_name='名字')
+    lastname = models.CharField(max_length=255,
+                                verbose_name='姓')
+    phone = models.IntegerField(null=True, 
+                                blank=True,
+                                verbose_name='電話')
+    joined_date = models.DateField(null=True,
+                                   blank=True,
+                                   verbose_name='入會日期',
+                                   default=datetime.date.today(),
+                                   help_text='the day he pay fee',
+                                   editable=True,
+                                   )
 
     def __str__(self):
       return f"{self.firstname} {self.lastname}"  
