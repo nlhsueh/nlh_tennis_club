9303e741fb8283587caa47264c12e9d2f85ae1ce
nienlinh <nlhsueh@mail.fcu.edu.tw>
2026-05-13 16:26:46 +0800

    UI: Apply Tailwind CSS v4 and add root redirect

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/members/', permanent=False)),
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
 ]
